39de971dbed491864bfdf6c88415c328100b4786
GiyoMoon <giyomoon@gmail.com>
2021-11-15 15:41:02 +0100

    :bug: Fix mistake in password regex

--- src/users/dtos/changePassword.dto.ts
+++ src/users/dtos/changePassword.dto.ts
@@ -5,7 +5,7 @@ export class UserChangePasswordDTO {
   oldPassword: string;
 
   @IsString()
-  @Matches(/^(?=.*[a-z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$/, {
+  @Matches(/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$/, {
     message: 'Password must be at least 8 characters long, contain at least 1 letter and 1 number'
   })
   password: string;
--- src/users/dtos/register.dto.ts
+++ src/users/dtos/register.dto.ts
@@ -8,7 +8,7 @@ export class UserRegisterDTO {
   username: string;
 
   @IsString()
-  @Matches(/^(?=.*[a-z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$/, {
+  @Matches(/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$/, {
     message: 'Password must be at least 8 characters long, contain at least 1 letter and 1 number'
   })
   password: string;
