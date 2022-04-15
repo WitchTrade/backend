4d4ff94b40184a6d17c30d5d097b24b1e7c9a437
GiyoMoon <giyomoon@gmail.com>
2022-04-15 20:45:48 +0200

    :technologist: Add new event ingredients to knownEventItems

--- src/static/knownEventItems.ts
+++ src/static/knownEventItems.ts
@@ -5,6 +5,8 @@ const knownEventItems = [
   { name: 'Filled Boot', tagSlot: 'chest' },
   { name: 'Gloomkin', tagSlot: 'chest' },
   { name: 'Cryptic Artifact', tagSlot: 'chest' },
+  { name: 'Colorful Egg', tagSlot: 'ingredient' },
+  { name: 'Spring Basket', tagSlot: 'ingredient' },
 ];
 
 export function isKnownEventItem(name: string, tagSlot: string): boolean {
